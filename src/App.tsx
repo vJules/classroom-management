631c350d70f8f234f213b8c061756020f787f1e8
Julian van Enckevort <julian.van.enckevort@gmail.com>
2023-01-03 12:53:00 +0100

    Added react-i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.scss'
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>{t('welcome')}</p>
         <a
           className='App-link'
           href='https://reactjs.org'
@@ -20,7 +21,7 @@ function App() {
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
